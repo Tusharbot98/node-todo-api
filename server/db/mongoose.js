31c36399b4aa2ce5fa6b9744923b731c557acf31
Tushar <tushar.nadkar98@gmail.com>
2018-10-15 12:34:47 +0530

    heroku app set

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,9 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URL ||'mongodb://Tushar141998:tu01sh04ar1998@ds131983.mlab.com:31983/todoapp',{ useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URL ||'mongodb://tushar:tu01sh04ar1998@ds131983.mlab.com:31983/todoapp',{ useNewUrlParser: true },(err)=>{
+    console.log("connection error + " + err)
+});
 module.exports={
     mongoose
 }
